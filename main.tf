eca8c9c9b983d1474262023d3804ea96f68b5458
gbgbcmrf86 <gbgbcmrf86@gmail.com>
2023-08-26 17:43:01 +0300

    added variables

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
 provider "kubernetes" {
@@ -9,8 +9,7 @@ provider "kubernetes" {
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
 }
 
@@ -20,8 +19,8 @@ provider "helm" {
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
       command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     }
   }
 }
@@ -35,29 +34,28 @@ locals {
 }
 
 module "vpc" {
-  source                  = "terraform-aws-modules/vpc/aws"
-  name                    = "dimav-eks-vpc"
-  cidr                    = "192.168.0.0/16"
-  azs                     = ["us-east-1b", "us-east-1c"]
-  private_subnets         = ["192.168.0.0/24", "192.168.1.0/24"]
-  public_subnets          = ["192.168.129.0/24", "192.168.130.0/24"]
+  source = "terraform-aws-modules/vpc/aws"
+
+  name                    = var.VPC_name
+  cidr                    = var.VPC_CIDR
+  azs                     = var.azs
+  private_subnets         = var.private_subnets
+  public_subnets          = var.public_subnets
   private_subnet_tags     = var.private_subnet_tags
   public_subnet_tags      = var.public_subnet_tags
-  enable_nat_gateway      = true
-  single_nat_gateway      = true
-  enable_vpn_gateway      = false
-  map_public_ip_on_launch = true
+  enable_nat_gateway      = var.eks_cluster_name
+  single_nat_gateway      = var.single_nat_gateway
+  enable_vpn_gateway      = var.enable_vpn_gateway
+  map_public_ip_on_launch = var.map_public_ip_on_launch
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
 
-  cluster_name    = var.eks_cluster_name
-  cluster_version = "1.27"
-
+  cluster_name                   = var.eks_cluster_name
+  cluster_version                = var.eks_cluster_version
   cluster_endpoint_public_access = true
-
   cluster_addons = {
     coredns = {
       most_recent = true
@@ -69,22 +67,20 @@ module "eks" {
       most_recent = true
     }
   }
-
   vpc_id     = local.vpc_id
   subnet_ids = concat(local.public_subnets_ids, local.private_subnets_ids)
 
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
-    instance_types = ["t3.medium"]
+    instance_types = ["t2.micro"]
   }
-
   eks_managed_node_groups = {
     dimav_tf_ng = {
       min_size     = 1
       max_size     = 3
       desired_size = 2
 
-      instance_types = ["t3.medium"]
+      instance_types = ["t2.micro"]
       #capacity_type  = "SPOT"
     }
   }
@@ -158,7 +154,6 @@ module "iam_iam-role-for-service-accounts-eks" {
 
   role_name                              = "dimav_eks_lb"
   attach_load_balancer_controller_policy = true
-
   oidc_providers = {
     main = {
       provider_arn               = module.eks.oidc_provider_arn
@@ -190,32 +185,26 @@ resource "helm_release" "lb" {
   depends_on = [
     kubernetes_service_account.service-account
   ]
-
   set {
     name  = "region"
     value = "us-east-1"
   }
-
   set {
     name  = "vpcId"
     value = local.vpc_id
   }
-
   set {
     name  = "image.repository"
     value = "602401143452.dkr.ecr.us-east-1.amazonaws.com/amazon/aws-load-balancer-controller"
   }
-
   set {
     name  = "serviceAccount.create"
     value = "false"
   }
-
   set {
     name  = "serviceAccount.name"
     value = "aws-load-balancer-controller"
   }
-
   set {
     name  = "clusterName"
     value = "dimav-tf-eks"
@@ -235,7 +224,6 @@ resource "kubernetes_service" "web-service" {
       name        = "http"
       target_port = 80
     }
-
     type = "NodePort"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,26 @@
+variable "region" {
+  description = "AWS Region"
+  type        = string
+  value       = "us-east-1"
+}
+
+# ------------- VPC -------------
+variable "azs" {
+  description = "A list of availability zones names or ids in the region"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "VPC_name" {
+  description = "VPC name"
+  type        = string
+  value       = "dimav-eks-vpc"
+}
+
 variable "private_subnet_tags" {
   description = "Additional tags for the private subnets"
   type        = map(string)
   default = {
-    tag1                                 = "kubernetes.io/role/internal-elb"
-    tag2                                 = "kubernetes.io/cluster/dimav-tf-eks"
     "kubernetes.io/role/internal-elb"    = 1
     "kubernetes.io/cluster/dimav-tf-eks" = "shared"
   }
@@ -13,15 +30,64 @@ variable "public_subnet_tags" {
   description = "Additional tags for the private subnets"
   type        = map(string)
   default = {
-    tag1                                 = "kubernetes.io/role/elb",
-    tag2                                 = "kubernetes.io/cluster/dimav-tf-eks"
     "kubernetes.io/role/elb"             = 1
     "kubernetes.io/cluster/dimav-tf-eks" = "shared"
   }
 }
 
+variable "VPC_CIDR" {
+  description = "VPC CIDR"
+  type        = string
+  value       = "192.168.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "A list of public subnets inside the VPC"
+  type        = list(string)
+  default     = ["192.168.129.0/24", "192.168.130.0/24"]
+}
+
+variable "private_subnets" {
+  description = "A list of private subnets inside the VPC"
+  type        = list(string)
+  default     = ["192.168.0.0/24", "192.168.1.0/24"]
+}
+
+variable "enable_nat_gateway" {
+  description = "Should be true if you want to provision NAT Gateways for each of your private networks"
+  type        = bool
+  default     = true
+}
+
+variable "single_nat_gateway" {
+  description = "Should be true if you want to provision a single shared NAT Gateway across all of your private networks"
+  type        = bool
+  default     = true
+}
+
+variable "enable_vpn_gateway" {
+  description = "Should be true if you want to create a new VPN Gateway resource and attach it to the VPC"
+  type        = bool
+  default     = false
+}
+
+variable "map_public_ip_on_launch" {
+  description = "Specify true to indicate that instances launched into the subnet should be assigned a public IP address. Default is `false`"
+  type        = bool
+  default     = true
+}
+
+
+#-------- EKS ----------
 variable "eks_cluster_name" {
-  description = "eks_cluster_name"
+  description = "AWS Kubernetes cluster name"
   type        = string
   default     = "dimav-tf-eks"
 }
+
+variable "eks_cluster_version" {
+  description = "AWS Kubernetes cluster version"
+  type        = string
+  default     = "1.27"
+}
+
