aaff8a3c8c321caa5b28206b68a985166a9d29f6
gbgbcmrf86 <gbgbcmrf86@gmail.com>
2023-08-28 23:05:11 +0300

    test Jenkins

--- main.tf
+++ main.tf
@@ -34,13 +34,13 @@ terraform {
   }
 }
 
-locals {
+/* locals {
   vpc_id              = module.vpc.vpc_id
   vpc_cidr            = module.vpc.vpc_cidr_block
   public_subnets_ids  = module.vpc.public_subnets
   private_subnets_ids = module.vpc.private_subnets
   subnets_ids         = concat(local.public_subnets_ids, local.private_subnets_ids)
-}
+} */
 
 /* module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
