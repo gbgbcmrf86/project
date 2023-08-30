de9bd3e50bc7906b54de64c46377a88999891caf
gbgbcmrf86 <gbgbcmrf86@gmail.com>
2023-08-30 12:15:08 +0300

    test GitWebHook

--- .terraform/modules/eks/variables.tf
+++ .terraform/modules/eks/variables.tf
@@ -553,13 +553,13 @@ variable "putin_khuylo" {
 variable "manage_aws_auth_configmap" {
   description = "Determines whether to manage the aws-auth configmap"
   type        = bool
-  default     = false //changed
+  default     = false
 }
 
 variable "create_aws_auth_configmap" {
   description = "Determines whether to create the aws-auth configmap. NOTE - this is only intended for scenarios where the configmap does not exist (i.e. - when using only self-managed node groups). Most users should use `manage_aws_auth_configmap`"
   type        = bool
-  default     = false //changed
+  default     = false
 }
 
 variable "aws_auth_node_iam_role_arns_non_windows" {
